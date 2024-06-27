c9ed2e83cfdf9bed0d651c23998fd65cd59db45f
pratik00020 <pratikkanojia0727@gmail.com>
2024-06-27 15:55:16 +0530

    main

--- VideoSearch/settings.py
+++ VideoSearch/settings.py
@@ -1,4 +1,13 @@
 CELERY_BROKE_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
-AWS_ACCESS_KEY_ID = ''
+AWS_ACCESS_KEY_ID = 'AKIA2UC27VLUHEW4SA6B'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'video-storage-bucket-pratik'
+DYNAMODB_TABLE_NAME = 'VideoSubtitle'
+
+
+
+# Static and media files settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
